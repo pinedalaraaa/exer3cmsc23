13faaa5561116941935b826917dad2763ca7a3d0
pinedalaraaa <lbpineda1@up.edu.ph>
2020-02-24 17:05:49 +0800

    with CountUp

--- exer3_pineda_b2l/app/src/main/java/com/android/example/exer3_pineda_b2l/MainActivity.kt
+++ exer3_pineda_b2l/app/src/main/java/com/android/example/exer3_pineda_b2l/MainActivity.kt
@@ -2,6 +2,7 @@ package com.android.example.exer3_pineda_b2l
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,74 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.graphics.toColor
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tileOne : TextView
+    lateinit var tileTwo : TextView
+    lateinit var tileThree : TextView
+    lateinit var tileFour : TextView
+    lateinit var tileFive : TextView
+
+    lateinit var tileSix : TextView
+    lateinit var tileSeven : TextView
+    lateinit var tileEight : TextView
+    lateinit var tileNine : TextView
+    lateinit var tileTen : TextView
+
+    lateinit var tileEleven : TextView
+    lateinit var tileTwelve : TextView
+    lateinit var tileThirteen : TextView
+    lateinit var tileFourteen : TextView
+    lateinit var tileFifteen : TextView
+
+    lateinit var tileSixteen : TextView
+    lateinit var tileSeventeen : TextView
+    lateinit var tileEighteen : TextView
+    lateinit var tileNineteen : TextView
+    lateinit var tileTwenty : TextView
+
+    lateinit var tileTwentyOne : TextView
+    lateinit var tileTwentyTwo : TextView
+    lateinit var tileTwentyThree : TextView
+    lateinit var tileTwentyFour : TextView
+    lateinit var tileTwentyFive : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tileOne = findViewById(R.id.tile_one)
+        tileTwo = findViewById(R.id.tile_two)
+        tileThree = findViewById(R.id.tile_three)
+        tileFour = findViewById(R.id.tile_four)
+        tileFive = findViewById(R.id.tile_five)
+
+        tileSix = findViewById(R.id.tile_six)
+        tileSeven = findViewById(R.id.tile_seven)
+        tileEight = findViewById(R.id.tile_eight)
+        tileNine = findViewById(R.id.tile_nine)
+        tileTen = findViewById(R.id.tile_ten)
+
+        tileEleven = findViewById(R.id.tile_eleven)
+        tileTwelve = findViewById(R.id.tile_twelve)
+        tileThirteen = findViewById(R.id.tile_thirteen)
+        tileFourteen = findViewById(R.id.tile_fourteen)
+        tileFifteen = findViewById(R.id.tile_fifteen)
+
+        tileSixteen = findViewById(R.id.tile_sixteen)
+        tileSeventeen = findViewById(R.id.tile_seventeen)
+        tileEighteen = findViewById(R.id.tile_eighteen)
+        tileNineteen = findViewById(R.id.tile_nineteen)
+        tileTwenty = findViewById(R.id.tile_twenty)
+
+        tileTwentyOne = findViewById(R.id.tile_twenty_one)
+        tileTwentyTwo = findViewById(R.id.tile_twenty_three)
+        tileTwentyThree = findViewById(R.id.tile_twenty_three)
+        tileTwentyFour = findViewById(R.id.tile_twenty_four)
+        tileTwentyFive = findViewById(R.id.tile_twenty_five)
+
 
         findViewById<Button>(R.id.start_button).setOnClickListener {
             addName(it)
@@ -24,6 +86,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //sets the player's name
     private fun addName(view: View) {
         val editText = findViewById<EditText>(R.id.name_edit)
         val nameTextView = findViewById<TextView>(R.id.display_name)
@@ -34,40 +97,21 @@ class MainActivity : AppCompatActivity() {
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
+    //updates the counter
+    private fun countUp() {
+        val newResult: TextView = findViewById(R.id.counter)
+
+        val text = newResult.getText().toString()
+        val number = Integer.parseInt(text)
+        val newCount = (number+1).toString()
+        newResult.setText(newCount)
+    }
+
+    //updates the interface once the user has clicked the start button
     private fun updateInterface (view: View) {
         val editText = findViewById<EditText>(R.id.name_edit)
         val startButton = findViewById<Button>(R.id.start_button)
         val gameTitle = findViewById<TextView>(R.id.game_title)
-
-        val tileOne = findViewById<TextView>(R.id.tile_one)
-        val tileTwo = findViewById<TextView>(R.id.tile_two)
-        val tileThree = findViewById<TextView>(R.id.tile_three)
-        val tileFour = findViewById<TextView>(R.id.tile_four)
-        val tileFive = findViewById<TextView>(R.id.tile_five)
-
-        val tileSix = findViewById<TextView>(R.id.tile_six)
-        val tileSeven = findViewById<TextView>(R.id.tile_seven)
-        val tileEight = findViewById<TextView>(R.id.tile_eight)
-        val tileNine = findViewById<TextView>(R.id.tile_nine)
-        val tileTen = findViewById<TextView>(R.id.tile_ten)
-
-        val tileEleven = findViewById<TextView>(R.id.tile_eleven)
-        val tileTwelve = findViewById<TextView>(R.id.tile_twelve)
-        val tileThirteen = findViewById<TextView>(R.id.tile_thirteen)
-        val tileFourteen = findViewById<TextView>(R.id.tile_fourteen)
-        val tileFifteen = findViewById<TextView>(R.id.tile_fifteen)
-
-        val tileSixteen = findViewById<TextView>(R.id.tile_sixteen)
-        val tileSeventeen = findViewById<TextView>(R.id.tile_seventeen)
-        val tileEighteen = findViewById<TextView>(R.id.tile_eighteen)
-        val tileNineteen = findViewById<TextView>(R.id.tile_nineteen)
-        val tileTwenty = findViewById<TextView>(R.id.tile_twenty)
-
-        val tileTwentyOne= findViewById<TextView>(R.id.tile_twenty_one)
-        val tileTwentyTwo = findViewById<TextView>(R.id.tile_twenty_two)
-        val tileTwentyThree = findViewById<TextView>(R.id.tile_twenty_three)
-        val tileTwentyFour = findViewById<TextView>(R.id.tile_twenty_four)
-        val tileTwentyFive = findViewById<TextView>(R.id.tile_twenty_five)
 
         val constraintGuide = findViewById<TextView>(R.id.constraint_guide)
         val displayName = findViewById<TextView>(R.id.display_name)
@@ -80,6 +124,7 @@ class MainActivity : AppCompatActivity() {
         gameTitle.visibility = View.GONE
         view.visibility = View.GONE
 
+        //elements of the updated interface
         constraintGuide.visibility = View.VISIBLE
         displayName.visibility = View.VISIBLE
         moveText.visibility = View.VISIBLE
@@ -117,86 +162,59 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //changes the color of the tiles
     private fun makeColored(view: View) {
+
         when (view.id) {
 
             // Boxes using Color class colors for the background
-            R.id.tile_one -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tile_two -> view.setBackgroundColor(Color.GRAY)
-            R.id.tile_three -> view.setBackgroundColor(Color.BLUE)
-            R.id.tile_four -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.tile_five -> view.setBackgroundColor(Color.BLUE)
+            R.id.tile_one -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_two -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_three -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_four -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_five -> view.setBackgroundColor(Color.BLACK)
 
-            R.id.tile_six -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tile_seven -> view.setBackgroundColor(Color.GRAY)
-            R.id.tile_eight -> view.setBackgroundColor(Color.BLUE)
-            R.id.tile_nine -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.tile_ten -> view.setBackgroundColor(Color.BLUE)
+            R.id.tile_six -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_seven -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_eight -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_nine -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_ten -> view.setBackgroundColor(Color.BLACK)
 
-            R.id.tile_eleven -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tile_twelve -> view.setBackgroundColor(Color.GRAY)
-            R.id.tile_thirteen -> view.setBackgroundColor(Color.BLUE)
-            R.id.tile_fourteen -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.tile_fifteen -> view.setBackgroundColor(Color.BLUE)
+            R.id.tile_eleven -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twelve -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_thirteen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_fourteen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_fifteen -> view.setBackgroundColor(Color.BLACK)
 
-            R.id.tile_sixteen -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tile_seventeen -> view.setBackgroundColor(Color.GRAY)
-            R.id.tile_eighteen -> view.setBackgroundColor(Color.BLUE)
-            R.id.tile_nineteen -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.tile_twenty -> view.setBackgroundColor(Color.BLUE)
+            R.id.tile_sixteen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_seventeen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_eighteen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_nineteen -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twenty -> view.setBackgroundColor(Color.BLACK)
 
-            R.id.tile_twenty_one -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.tile_twenty_two -> view.setBackgroundColor(Color.GRAY)
-            R.id.tile_twenty_three -> view.setBackgroundColor(Color.BLUE)
-            R.id.tile_twenty_four -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.tile_twenty_five -> view.setBackgroundColor(Color.BLUE)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.tile_twenty_one -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twenty_two -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twenty_three -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twenty_four -> view.setBackgroundColor(Color.BLACK)
+            R.id.tile_twenty_five -> view.setBackgroundColor(Color.BLACK)
         }
     }
+
+    //sets the listeners to the root layout and each tile
     private fun setListeners() {
 
-        val tileOne = findViewById<TextView>(R.id.tile_one)
-        val tileTwo = findViewById<TextView>(R.id.tile_two)
-        val tileThree = findViewById<TextView>(R.id.tile_three)
-        val tileFour = findViewById<TextView>(R.id.tile_four)
-        val tileFive = findViewById<TextView>(R.id.tile_five)
-
-        val tileSix = findViewById<TextView>(R.id.tile_six)
-        val tileSeven = findViewById<TextView>(R.id.tile_seven)
-        val tileEight = findViewById<TextView>(R.id.tile_eight)
-        val tileNine = findViewById<TextView>(R.id.tile_nine)
-        val tileTen = findViewById<TextView>(R.id.tile_ten)
-
-        val tileEleven = findViewById<TextView>(R.id.tile_eleven)
-        val tileTwelve = findViewById<TextView>(R.id.tile_twelve)
-        val tileThirteen = findViewById<TextView>(R.id.tile_thirteen)
-        val tileFourteen = findViewById<TextView>(R.id.tile_fourteen)
-        val tileFifteen = findViewById<TextView>(R.id.tile_fifteen)
-
-        val tileSixteen = findViewById<TextView>(R.id.tile_sixteen)
-        val tileSeventeen = findViewById<TextView>(R.id.tile_seventeen)
-        val tileEighteen = findViewById<TextView>(R.id.tile_eighteen)
-        val tileNineteen = findViewById<TextView>(R.id.tile_nineteen)
-        val tileTwenty = findViewById<TextView>(R.id.tile_twenty)
-
-        val tileTwentyOne= findViewById<TextView>(R.id.tile_twenty_one)
-        val tileTwentyTwo = findViewById<TextView>(R.id.tile_twenty_two)
-        val tileTwentyThree = findViewById<TextView>(R.id.tile_twenty_three)
-        val tileTwentyFour = findViewById<TextView>(R.id.tile_twenty_four)
-        val tileTwentyFive = findViewById<TextView>(R.id.tile_twenty_five)
-
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        rootConstraintLayout.setBackgroundColor(Color.LTGRAY)
 
         val clickableViews: List<View> =
             listOf(tileOne, tileTwo, tileThree, tileFour, tileFive,
                 tileSix, tileSeven, tileEight, tileNine, tileTen,
                 tileEleven, tileTwelve, tileThirteen, tileFourteen, tileFifteen,
                 tileSixteen, tileSeventeen, tileEighteen, tileNineteen, tileTwenty,
-                tileTwentyOne, tileTwentyTwo, tileTwentyThree, tileTwentyFour, tileTwentyFive,
-                rootConstraintLayout)
+                tileTwentyOne, tileTwentyTwo, tileTwentyThree, tileTwentyFour, tileTwentyFive)
 
         for (item in clickableViews) {
+            item.setOnClickListener { countUp() }
             item.setOnClickListener { makeColored(it) }
         }
     }
